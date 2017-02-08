2f283dff7d07c2fa94d9102834292cf61cb3feaf
Klvst3r <klvst3r@gmail.com>
2017-02-07 19:43:07 -0600

    Create Index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: Login PHP :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>PHP Register and Login Basic System</h2>
+			<p class="lead">Register sistem with PHP and MySQL</p>
+			
+			<p>Instructions:</p>
+			<ol>
+				<li>Click in Sign Up.</li>
+				<li>Begin Session in Log In ooption.</li>
+				<li>To end session, lick in exit.</li>
+			</ol>
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
