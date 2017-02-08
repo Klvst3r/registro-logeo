050f49ecb60f02ed612ae644c256250705545c93
Klvst3r <klvst3r@gmail.com>
2017-02-07 23:03:39 -0600

    Create SignIn Form and Select User File

--- login.php
+++ login.php
@@ -1,7 +1,11 @@
 <?php session_start(); ?>
-<html>
+<!DOCTYPE html>
+<html lang="es_MX">
 	<head>
-		<title>Formulario de Registro</title>
+		<title>Sign In</title>
+		
+		<meta charset ="utf-8"/>
+
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	</head>
 	<body>
@@ -11,14 +15,14 @@
 <div class="col-md-6">
 		<h2>Login</h2>
 
-		<form role="form" name="login" action="php/login.php" method="post">
+		<form role="form" name="login" action="php/signin.php" method="post">
 		  <div class="form-group">
 		    <label for="username">Nombre de usuario o email</label>
 		    <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de usuario">
 		  </div>
 		  <div class="form-group">
-		    <label for="password">Contrase&ntilde;a</label>
-		    <input type="password" class="form-control" id="password" name="password" placeholder="Contrase&ntilde;a">
+		    <label for="password">Contraseña</label>
+		    <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
 		  </div>
 
 		  <button type="submit" class="btn btn-default">Acceder</button>
@@ -26,6 +30,6 @@
 </div>
 </div>
 </div>
-		<script src="js/valida_login.js"></script>
+		<script src="js/valida_signin.js"></script>
 	</body>
 </html>
